d541e8f5354faeac8093598cdf4520f429d04c4b
lw3b3r <daevahush@gmail.com>
2022-07-13 14:02:13 -0400

    generated folder and file structure for notification feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 import { GroupModule } from './group/group.module';
 import { AuthModule } from './auth/auth.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [GameModule, GroupModule, AuthModule],
+  imports: [GameModule, GroupModule, AuthModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
